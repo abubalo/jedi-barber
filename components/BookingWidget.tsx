e7394bddf640a0693af904b0d2865f45cd7d01f5
Abubakar Balogun <42701866+abubalo@users.noreply.github.com>
2023-04-09 22:19:44 +0200

    Modified

--- components/BookingWidget.tsx
+++ components/BookingWidget.tsx
@@ -7,7 +7,7 @@ type Props = {};
     }
 
 const BookingWidget = (props: Props) => {
-  return <div className="w-full h-4/5 p-3 bg-white md:w-1/4 text-black">
+  return <div className="w-full p-3 text-black bg-white rounded-md h-4/5 md:w-1/4">
    <p> hello there</p>
    <Button onClick={handleClick} color={"bg-green-500"} type="button"/>
   </div>;
