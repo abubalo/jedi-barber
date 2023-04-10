895c43ded971ebe7c85b2c682987a8235503c4fc
Abubakar Balogun <42701866+abubalo@users.noreply.github.com>
2023-04-10 16:58:30 +0200

    Back Icon

---
+++ components/icons/BackwardIcon.tsx
@@ -0,0 +1,26 @@
+import React from "react";
+
+type Props = {};
+
+const BackwardIcon = (props: Props) => {
+  return (
+    <div className="cursor-pointer">
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        fill="none"
+        viewBox="0 0 24 24"
+        strokeWidth={1.5}
+        stroke="currentColor"
+        className="w-6 h-6"
+      >
+        <path
+          strokeLinecap="round"
+          strokeLinejoin="round"
+          d="M15.75 19.5L8.25 12l7.5-7.5"
+        />
+      </svg>
+    </div>
+  );
+};
+
+export default BackwardIcon;
