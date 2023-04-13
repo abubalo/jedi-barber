1218c6b4356e6d02ba8b56ea75a7aea5f523bbce
Abubakar Balogun <42701866+abubalo@users.noreply.github.com>
2023-04-13 23:17:55 +0200

    Deleted uneccessary components

---
+++ components/icons/PrevIcon.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+type Props = {};
+
+const PrevIcon = (props: Props) => {
+  return (
+    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
+  <path strokeLinecap="round" strokeLinejoin="round" d="M15.75 19.5L8.25 12l7.5-7.5" />
+</svg>
+
+);
+  
+};
+
+export default PrevIcon;
