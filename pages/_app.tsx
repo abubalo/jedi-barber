195ea9cb1e32d902751ab5b51069700ef968c196
Abubakar Balogun <42701866+abubalo@users.noreply.github.com>
2023-04-24 15:08:57 +0200

    Removed custom font from app.tsx

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,24 +1,6 @@
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
 import Layout from "@/components/Layout";
-import Head from "next/head";
-import { Space_Grotesk } from "@next/font/google";
-import { ReactNode } from "react";
-import Header from "@/components/Header";
-
-// const globalStyles = css`
-//   @font-face {
-//     font-family: "Space Grotesk";
-//     font-style: normal;
-//     font-weight: 400;
-//     src: url(${Space_Grotesk[400].woff2}) format("woff2");
-//   }
-
-//   body {
-//     font-family: "Space Grotesk", sans-serif;
-//   }
-// `;
-
 
 export default function App({ Component, pageProps }: AppProps) {
   if (Component.name === "dashboard" || Component.name === "booking") {
