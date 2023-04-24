30c6c3589001dedf2b85e66cdc0a090d43ae4577
Abubakar Balogun <42701866+abubalo@users.noreply.github.com>
2023-04-24 11:30:55 +0200

    Create mongoose connection

--- pages/api/config/initMongoose.ts
+++ pages/api/config/initMongoose.ts
@@ -1,3 +1,18 @@
 import mongoose from "mongoose";
 
-expor default function
+export async function mongoConfig() {
+  const mongoUrl = process.env.MONGODB_URL;
+
+  if (!mongoUrl) {
+    throw new Error("MongoDB URL is not defined");
+  }
+  mongoose.set({ strictQuery: true });
+  mongoose.connect(mongoUrl);
+
+  const db = mongoose.connection;
+
+  db.on("error", console.error.bind(console, "MongoDB connection error:"));
+  db.once("open", () => {
+    console.log("MongoDB connected!");
+  });
+}
