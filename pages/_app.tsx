ad82f688ec203d6f834551e7bee5247ea8cc345b
Abubakar Balogun <42701866+abubalo@users.noreply.github.com>
2023-05-24 16:26:13 +0200

    fix getLaout issue

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,10 +2,16 @@ import "@/styles/globals.css";
 import type { AppProps } from "next/app";
 import Layout from "@/components/Layout";
 
-export default function App({ Component, pageProps }: AppProps) {
-  if (Component.name === "dashboard" || Component.name === "booking") {
-    return <Component {...pageProps} />;
+export default function App({ Component, pageProps, router }: AppProps) {
+  // if (Component.name === "dashboard" || Component.name === "booking") {
+  //   return <Component {...pageProps} />;
+  // }
+  
+  if(router.pathname.startsWith("/dashboard") || router.pathname.startsWith("/dashboard")){
+      return <Component {...pageProps} />;
+
   }
+
   return (
     <Layout>
       <Component {...pageProps} />
