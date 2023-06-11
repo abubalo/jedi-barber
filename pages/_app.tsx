41059f11c78d613c20c0693065827e2fe106f265
Abubakar Balogun <42701866+abubalo@users.noreply.github.com>
2023-06-11 23:10:49 +0200

    add persistent layout to _app.tsx

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,20 +1,16 @@
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
 import Layout from "@/components/Layout";
-import { Page } from "./types/page";
+import { Page } from "../types/page";
 import { Fragment } from "react";
 
 type Props = AppProps & {
   Component: Page;
-}
+};
 
-export default function App({ Component, pageProps, router }: Props) {
-  const getLayout = Component.getLayout ?? (page => page);
-  const Layout = Component.layout ?? Fragment
+export default function App({ Component, pageProps }: Props) {
+  const getLayout = Component.getLayout ?? ((page) => page);
+  const Layout = Component.layout ?? Fragment;
 
-  return (
-    <Layout>
-      {getLayout(<Component {...pageProps} />)}
-    </Layout>
-  );
+  return <Layout>{getLayout(<Component {...pageProps} />)}</Layout>;
 }
