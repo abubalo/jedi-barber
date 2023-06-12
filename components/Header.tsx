a26f14e7ff17c289e813d3cb26b04e06d1ef3153
Abubakar Balogun <42701866+abubalo@users.noreply.github.com>
2023-06-12 23:34:26 +0200

    add more nav menu

--- components/Header.tsx
+++ components/Header.tsx
@@ -29,6 +29,8 @@ function Header({}: Props) {
       <span>
         <Image src="/assets/jedi-logo.png" width={100} height={100} alt="jedi logo" />
       </span>
+      <Link href={"/about"} className="px-8 py-3 transition-all duration-150 border-2 border-white rounded-full cursor-pointer hover:text-black hover:bg-white">About</Link>
+      <Link href={"/consultation"} className="px-8 py-3 transition-all duration-150 border-2 border-white rounded-full cursor-pointer hover:text-black hover:bg-white">Consultation</Link>
       <Link href={"/booking"} className="px-8 py-3 transition-all duration-150 border-2 border-white rounded-full cursor-pointer hover:text-black hover:bg-white">Book now</Link>
       {/* <Button type="button" onClick={smm} color={"text-green-300"} text={"Sign up"} bg={"bg-purple-500"}/> */}
     </header>
