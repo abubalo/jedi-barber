cdfdb85b94b20d82bfefa02d293412c7649e76e8
Abubakar Balogun <42701866+abubalo@users.noreply.github.com>
2023-07-05 21:57:22 +0200

    reafactor: made booking page responsive

--- pages/booking.tsx
+++ pages/booking.tsx
@@ -7,9 +7,11 @@ type Props = {};
 const booking = (props: Props) => {
   return (
     <div className="text-white bg-black bg-center bg-cover bg-hero2">
-      <Overlay />
+      <div className="hidden md:block">
+        <Overlay />
+      </div>
       <div className="container flex items-center justify-between w-full h-screen mx-auto">
-        <h1 className="z-10 text-2xl font-bold">Get best style you can</h1>
+        <h1 className="hidden z-10 text-2xl font-bold md:block">Get best style you can</h1>
         <BookingWidget />
       </div>
     </div>
